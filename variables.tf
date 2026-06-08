95b7cc7af9b2a88f42dfc3cc8a4e973835acfc99
TheJourneysEnd <wffafyfc1@gmail.com>
2026-06-08 20:48:02 +0300

    fix: замена пути на относительный

--- variables.tf
+++ variables.tf
@@ -5,3 +5,8 @@ variable "sa-id" {
 variable "yc_token" { type = string }
 variable "yc_cloud_id" { type = string }
 variable "yc_folder_id" { type = string }
+variable "ssh_public_key_path" {
+  type        = string
+  description = "Путь к публичному SSH-ключу"
+  default     = "~/.ssh/id_rsa.pub"
+}
